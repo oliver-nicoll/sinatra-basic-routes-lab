aad350c6649a297b58a85ff86328c8d6aa6fd806
Nicoll Oliver <nicolldon96@gmail.com>
2021-03-11 13:48:56 -0500

    commit tree 753fdbb55170ab01aa91628303f17e4795c1f656

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Nicoll"
+    end
+
+    get '/hometown' do
+        "My hometown is Lima, Peru... but it's actually Berwick, Pa."
+    end
+
+    get '/favorite-song' do
+        "My favorite song is honestly anything... I listen to anything and everything."
+    end
 end
